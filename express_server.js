ce4f61f602aa0ac3c33617fb3cf72a9353dc732a
Shameer Meerambigari <shameeritsme21@gmail.com>
2025-12-21 22:17:23 +0530

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 const app = express();
-app.get('/users', (req, res) => {
+app.get('/users', (req, res) => 
+{
   res.send('hello from users page!');
 });
+
 const PORT = process.env.PORT || 8006;
-app.listen(PORT, () => {
+app.listen(PORT, () => 
+{
   console.log(`server is running at port ${PORT}`);
 });
